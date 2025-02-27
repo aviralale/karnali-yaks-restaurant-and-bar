5df36d323105bab71a4502eb468c53a52582411d
Aviral Ale <aviralale@gmail.com>
2025-02-27 21:38:43 +0545

    Replace HeroMenu images and update content to highlight Karnali region; remove MenuItem component

--- src/components/HeroMenu.tsx
+++ src/components/HeroMenu.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
-import ChickenTandooriImg from "../assets/images/chicken-tandoori.png";
-import FriedRiceImg from "../assets/images/fried-rice.png";
-import MomoImg from "../assets/images/moom.png";
+import NepalImg from "@/assets/images/nepal.png";
+import YakImg from "@/assets/images/yak.png";
+import LaliguransImg from "../assets/images/laligurans.png";
+import MtEverestImg from "../assets/images/—Pngtree—everest mount nepal_14553171.png";
 
 const HeroMenu = () => {
   return (
@@ -14,25 +15,41 @@ const HeroMenu = () => {
             padding: "1rem",
           }}
         >
-          <span className="flex flex-col md:flex-row items-start md:items-end gap-4 relative">
-            EXPERIENCE OF REAL RECIPES TASTE
-            <img
-              src={ChickenTandooriImg}
-              className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 self-end md:absolute md:-right-16 lg:-translate-x-24 xl:-translate-x-96"
-              alt="Chicken Tandoori"
-            />
+          <span className="flex  md:flex-row items-start md:items-end gap-4 relative">
+            ABOUT KARNALI
+            <img src={NepalImg} className="w-48 sm:w-48 md:w-96" alt="Nepal" />
           </span>
         </h1>
         <div className="flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-4">
-          <img
-            src={FriedRiceImg}
-            alt="Fried Rice"
-            className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
-          />
-          <span className="text-center sm:text-left sm:w-1/2 text-sm md:text-base">
-            But our menu doesn't stop there! We offer a wide range of cuisines,
-            from Nepal and the Himalayas to Asia and Mexico, ensuring something
-            for every palate. Explore global flavors with us!
+          <div className="flex flex-row sm:flex-row md:flex-col items-center gap-2">
+            <img
+              src={YakImg}
+              alt="Fried Rice"
+              className="w-32 sm:w-28 md:w-32"
+            />
+            <img
+              src={LaliguransImg}
+              alt="Fried Rice"
+              className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
+            />
+          </div>
+          <span
+            className="text-center sm:text-left sm:w-3/4 md:text-base"
+            style={{
+              fontSize: "1.3rem",
+            }}
+          >
+            Karnali is a region in Nepal known for its rich natural beauty,
+            diverse culture, and historical significance. It is both a province
+            (Karnali Province) and the name of Nepal’s longest and largest
+            river, the Karnali River.
+            <br />
+            <em className="text-emerald-800" style={{ margin: "1.2rem 0" }}>
+              Karnali es una región de Nepal conocida por su rica belleza
+              natural, su cultura diversa y su importancia histórica. Es a la
+              vez una provincia (provincia de Karnali) y el nombre del río más
+              largo y caudaloso de Nepal, el río Karnali.
+            </em>
           </span>
           <Link
             className="underline whitespace-nowrap font-semibold text-sm md:text-base"
@@ -46,8 +63,8 @@ const HeroMenu = () => {
       {/* Right Section - Image */}
       <div className="w-full md:w-1/2 flex justify-center items-center mt-4 md:mt-0">
         <img
-          src={MomoImg}
-          alt="Momo"
+          src={MtEverestImg}
+          alt="Laligurans"
           className="w-full max-w-md md:max-w-full aspect-square"
         />
       </div>
